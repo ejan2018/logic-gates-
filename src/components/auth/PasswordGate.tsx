95e8513638d72b6b061a8923a618b193771d1242
ejan2018 <redroseaamir@gmail.com>
2026-06-18 20:25:50 +0300

    Fix spelling error in PasswordGate component
    Corrected spelling of 'International' in the access code prompt.

--- src/components/auth/PasswordGate.tsx
+++ src/components/auth/PasswordGate.tsx
@@ -171,10 +171,9 @@ export function PasswordGate() {
             marginBottom: 28,
           }}
         >
-          Enter the 4-character access code to continue </br>
-        <strong> "Write the Initialise word of Pakistan Internation School Al-khobar"</strong>
-          
-        </p>
+          Enter the 4-character access code to continue <br />
+  <strong>"Write the Initialise word of Pakistan International School Al-khobar"</strong>
+</p>
 
         {/* Input boxes */}
         <div
